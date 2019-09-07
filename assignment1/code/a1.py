8479826417ebf61150cd349b506fdae444fb98bb
mortvest <dmitry.serykh@gmail.com>
2019-09-07 18:58:39 +0200

    code for ex1, kinda works

--- assignment1/code/a1.py
+++ assignment1/code/a1.py
@@ -1,12 +1,22 @@
 import numpy as np
 
 class GridWorld():
-    def __init__(self, size_x, size_y, rew_mat, trans_mat, init_vals=0, policy=None):
+    """ Grid world object
+
+    Args:
+        size_x (int): first dimension of the world
+        size_y (int): second dimension of the world
+        rew_mat (numpy array): matrix of rewards
+        trans_mat (numpy array): transition matrix
+        init_vals (int or np.array): initial values for the Vs
+        policy (numpy array): policy matrix, set to random by default
+    """
+    def __init__(self, size_x, size_y, rew_mat, trans_mat, init_vals=None, policy=None):
         self.size_x = size_x
         self.size_y = size_y
         flat_size = size_x * size_y
-        if isinstance(init_vals, int):
-            self.Vs = np.full(flat_size, init_vals)
+        if policy is None:
+            self.Vs = np.full(flat_size, 0.0)
         else:
             self.Vs = init_vals
         self.rew_mat = rew_mat
@@ -17,32 +27,59 @@ class GridWorld():
             self.policy = policy
 
     def __str__(self):
-        def gen_line(vals):
-            retval = "||"
-            for v in vals:
-                retval += " " + str(v) + " ||"
-            return retval
-        width = 2 + self.size_x * 5
-        h_line = "=" * width
-        retval = h_line
-        for row in range(self.size_y):
-            retval += "\n" + gen_line(self.Vs[row * self.size_x : (row + 1) * self.size_x])
-            retval += "\n" + h_line
-        return retval
+        return str(self.Vs.reshape(self.size_y, self.size_x))
 
     def evaluate_policy(self, theta):
-        delta = 0
-        while delta < theta:
+        """
+        Evaluates policy using dynamic programming
+        Args:
+            theta (float): threshold value
+        """
+        if debug: print("Evaluating policy")
+        while True:
+            delta = 0
             for s in range(self.size_x * self.size_y):
                 # update
                 v = self.Vs[s]
                 self.Vs[s] = np.sum(self.policy[s] * (self.rew_mat[s] + self.Vs[self.trans_mat[s]]))
                 delta = max(delta, abs(v - self.Vs[s]))
+            if delta < theta:
+                break
+
+    def iterate_policy(self, theta):
+        """
+        Iterates policy using dynamic programming
+        Args:
+            theta (float): threshold value for the policy evaluation
+        """
+        def find_value(s):
+            diag_mat = np.diag(np.ones(4))
+            diag_mult = diag_mat * (self.rew_mat[s] + self.Vs[self.trans_mat[s]])
+            if debug: print(diag_mult)
+            arr = np.diag(diag_mult)
+            if debug: print(arr)
+            max_ind = np.argmax(arr)
+            if debug: print(max_ind)
+            res_policy = diag_mat[max_ind]
+            if debug: print(res_policy)
+            return res_policy
+
+        f_stable = False
+        while not f_stable:
+            if debug: print("Iterating")
+            self.evaluate_policy(theta)
+            f_stable = True
+            for s in range(self.size_x * self.size_y):
+                a = np.copy(self.policy[s])
+                self.policy[s] = find_value(s)
+                if not np.array_equal(a, self.policy[s]):
+                    f_stable = False
 
 def main():
-    # init_vals = np.arange(0,12)
+    ## SETUP
+    # setup the transition and reward mappings
     init_vals = 0
-    actions = ["up", "down", "right", "left"]
+    # actions = ["up", "down", "right", "left"]
     reward_mapping = np.array([[-1, -1, -1, -1],
                                [-1, -6, -1, -1],
                                [-1, -1, -1, -1],
@@ -69,10 +106,39 @@ def main():
                                [7, 10, 11, 10],
                                [11,11, 11, 11]])
 
+    ## Sub-task 1
+    print("TASK 1")
+
+    # create a 3x4 grid world object with a random policy
     floorplan = GridWorld(3, 4, reward_mapping, trans_mapping, init_vals)
-    print(floorplan)
-    floorplan.evaluate_policy(0.5)
+
+    # evaluate policy
+    floorplan.evaluate_policy(0.01)
+
+    # view the resulting 12 values of Vrand
+    print("Value function of a random policy")
     print(floorplan)
 
+
+    ## Sub-task 2
+    print("")
+    print("TASK 2")
+
+    # create a 3x4 grid world object with a random default policy
+    floorplan = GridWorld(3, 4, reward_mapping, trans_mapping, init_vals)
+    print("Starting policy:")
+    print(floorplan.policy)
+
+    # iterate policy
+    floorplan.iterate_policy(0.01)
+    print("Optimal policy:")
+    print(floorplan.policy)
+
+    # view the resulting 12 values of V*
+    print("Optimal value function:")
+    print(floorplan)
+
+
 if __name__ == "__main__":
+    debug = False
     main()
