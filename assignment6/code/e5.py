64957699af842761ad2c0d4bdcfdd75207663f75
mortvest <dmitry.serykh@gmail.com>
2019-10-20 22:08:56 +0200

    plotting stuff

--- assignment6/code/e5.py
+++ assignment6/code/e5.py
@@ -11,6 +11,8 @@ class Plot():
         self.K = K
         self.n_reps = n_reps
 
+    def label(self):
+        pass
     def plot(self):
         acc = np.zeros(self.T)
         xs = np.linspace(1, self.T, self.T, dtype=int)
@@ -18,10 +20,12 @@ class Plot():
             x = self.calc_regrets(self.T, self.K, self.mu_star, self.mu)
             acc += x
         ys = acc/self.n_reps
-        plt.plot(xs, ys)
+        plt.plot(xs, ys, label=self.label())
 
     def calc_regrets(self, T, K, mu_star, mu):
         """implement for each type of plot"""
+        pass
+    def print_status(self):
         pass
 
 class UCB1(Plot):
@@ -32,7 +36,7 @@ class UCB1(Plot):
         """implement for each type of UCB1"""
         pass
     def __UCB1_generic(self, T, K, mu_star, mu, bound):
-        # wind-up
+        # initialization
         init_avg = [one_zero_with_prob(mu_star)]
         for _ in range(K-1):
             init_avg.append(one_zero_with_prob(mu))
@@ -59,12 +63,38 @@ class UCB1(Plot):
             regrets[t-1] = emp_reg
         return regrets
 
+
 class UCB1_notes(UCB1):
+    def label(self):
+        return "UCB from the lecture notes"
     def bound(self, t, N):
         return np.sqrt((3 * np.log(t))/(2 * N))
+    def print_status(self):
+        print("UCB notes for K={}, mu={}".format(self.K, self.mu))
 
-def eta_t(K, t):
-    return np.sqrt(np.log(K)/(t * K))
+class UCB1_improved(UCB1):
+    def label(self):
+        return "Improved UCB1"
+    def print_status(self):
+        print("UCB improved for K={}, mu={}".format(self.K, self.mu))
+    def bound(self, t, N):
+        return np.sqrt(np.log(t)/N)
+
+class EXP3(Plot):
+    def label(self):
+        return "EXP3"
+    def __eta_t(self, K, t):
+        return np.sqrt(np.log(K)/(t * K))
+    def calc_regrets(self, T, K, mu_star, mu):
+        # init data
+        Ls = np.zeros(K)
+        ps = np.zeros(K)
+        # iteration
+        for t in range(1, T+1):
+            pass
+        # eta = self.__eta_t(K, t)
+
+
 
 def empirical_regret(N_ts, deltas):
     return np.sum(N_ts * deltas)
@@ -72,63 +102,22 @@ def empirical_regret(N_ts, deltas):
 def one_zero_with_prob(prob):
     return np.random.choice(2, p=[1-prob, prob])
 
-# def UCB1_generic(T, K, mu_star, mu, bound):
-#     # wind-up
-#     init_avg = [one_zero_with_prob(mu_star)]
-#     for _ in range(K-1):
-#         init_avg.append(one_zero_with_prob(mu))
-#     avg_rewards = np.array(init_avg).astype(float)
-#     probs = np.array([mu_star] + [mu] * (K-1))
-#     Ns = np.ones(K)
-#     regrets = np.zeros(T)
-#     # normal runs
-#     for t in range(1, T+1):
-#         # apply the bound
-#         curr_avgs = bound(t, Ns) + avg_rewards
-#         # find the best hand
-#         best_ind = np.argsort(curr_avgs)[-1]
-#         # play the hand
-#         gain = one_zero_with_prob(probs[best_ind])
-#         N_old = Ns[best_ind]
-#         avg_old = avg_rewards[best_ind]
-#         # update average
-#         avg_rewards[best_ind] = (avg_old * N_old + gain) / float((N_old + 1))
-#         # update times played
-#         Ns[best_ind] += 1
-#         # calculate the regret
-#         emp_reg = empirical_regret(Ns, (mu_star - probs))
-#         regrets[t-1] = emp_reg
-#     return regrets
-
-
-def UCB1_plot_generic(T, K, mu_star, mu, n_reps, bound):
-    acc = np.zeros(T)
-    print("T =", T)
-    xs = np.linspace(1, T, T, dtype=int)
-    print(xs.shape)
-    for n in range(n_reps):
-        acc += UCB1_generic(T, K, mu_star, mu, bound)
-    ys = acc/n_reps
-    print(ys.shape)
-    print(xs.shape)
-    plt.plot(xs, ys)
-    plt.show()
-
-
-def UCB1_plot_notes(T, K, mu_star, mu, n_reps):
-    def bound(t, N):
-        return np.sqrt((3 * np.log(t))/(2 * N))
-    return UCB1_plot_generic(T, K, mu_star, mu, n_reps, bound)
-
-
-def EXP3(t):
-    pass
-
 def plot(T, mu_star, K, mu, n_reps):
-    print("Plotting for mu={}".format(mu))
-    print("UCB slides for T={}, mu_star={}, K={}, mu={}, n_reps={}". format(T, mu_star, K, mu, n_reps))
-    print("UCB improved T={}, mu_star={}, K={}, mu={}, n_reps={}". format(T, mu_star, K, mu, n_reps))
-    print("EXP3 T={}, mu_star={}, K={}, mu={}, n_reps={}". format(T, mu_star, K, mu, n_reps))
+    if debug: print("Plotting for mu={}".format(mu))
+    # define plot types
+    plots = [UCB1_notes(T, mu_star, mu, K, n_reps),
+             UCB1_improved(T, mu_star, mu, K, n_reps)]
+             # EXP3(T, mu_star, mu, K, n_reps)]
+    # plot each type
+    for p in plots:
+        if debug: p.print_status()
+        p.plot()
+    # define plot parameters
+    plt.xlabel("t")
+    plt.ylabel("regret")
+    plt.legend()
+    plt.title(r"$\mu={}, K={}$".format(mu, K))
+    plt.show()
 
 def main():
     T = 10000
@@ -138,16 +127,14 @@ def main():
     mus = [mu_star - mu for mu in mu_diffs]
     n_reps = 10
 
-    # UCB1_plot_notes(T, Ks[1], mu_star, mus[1], n_reps)
-    # print(UCB1_notes(T, Ks[1], mu_star, mus[1]))
-    # UCB1_improved(T, Ks[1], mu_star, mus[1])
+    # plot(T, mu_star, Ks[1], mus[1], n_reps)
+    x = EXP3(T, mu_star, mus[1], Ks[1], n_reps)
+    print(x.calc_regrets)
+
     # for mu in mus:
     #     for K in Ks:
     #         plot(T, mu_star, K, mu, n_reps)
-    ucb_test = UCB1_notes(T, mu_star, mus[1], Ks[1], n_reps)
-    ucb_test.plot()
-    plt.show()
 
 if __name__ == "__main__":
-    debug = False
+    debug = True
     main()
