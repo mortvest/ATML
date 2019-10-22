fa92bf69318bcbcda302f680b441dd611b22081c
mortvest <dmitry.serykh@gmail.com>
2019-10-22 12:05:19 +0200

    exp3 not working

--- assignment6/code/e5.py
+++ assignment6/code/e5.py
@@ -17,15 +17,17 @@ class Plot():
         pass
 
     def plot(self, with_stds=True):
-        acc = np.zeros(self.T)
-        xs = np.linspace(1, self.T, self.T, dtype=int)
-        for n in range(self.n_reps):
+        n = self.T-self.K
+        acc = []
+        xs = np.linspace(self.K, self.T, n, dtype=int)
+        for _ in range(self.n_reps):
             x = self.calc_regrets(self.T, self.K, self.mu_star, self.mu, self.adv_seq)
-            acc = np.vstack((acc, x))
-        acc = acc[1:]
+            acc.append(x)
+        acc = np.array(acc)
         ys = np.mean(acc, axis=0)
         # main plot
         plt.plot(xs, ys, label=self.label(), color=self.color)
+        # plot + std
         if with_stds:
             std = np.std(acc, axis=0)
             plt.plot(xs,
@@ -36,18 +38,15 @@ class Plot():
 
     def plot_adv(self):
         n = self.T-self.K
-        acc = np.zeros(n)
+        acc = []
         xs = np.linspace(self.K, self.T, n, dtype=int)
-        for n in range(self.n_reps):
+        for _ in range(self.n_reps):
             x = self.calc_regrets(self.T, self.K, self.mu_star, self.mu, self.adv_seq)
-            acc = np.vstack((acc, x))
-        acc = acc[1:]
+            acc.append(x)
+        acc = np.array(acc)
         best_column_ind = np.argsort(np.sum(self.adv_seq, axis=0))[-1]
         best_column = self.adv_seq[:,best_column_ind]
         cumsum = np.cumsum(best_column)
-
-        print(acc[-1,-1])
-        print(cumsum[-1])
         diff = np.subtract(cumsum, acc)
         ys = np.mean(diff, axis=0)
         plt.plot(xs, ys, label=self.label(), color=self.color)
@@ -77,15 +76,15 @@ class UCB1(Plot):
             avg_rewards = np.array(init_avg).astype(float)
             probs = np.array([mu_star] + [mu] * (K-1))
         else:
+            # play the first hand twice and get 1 twice
             avg_rewards = np.array([1] + [0]*(K-1)).astype(float)
 
         regrets = np.zeros(T-K)
         Ns = np.ones(K)
         # normal runs
         for t in range(K+1, T+1):
+            n = T - K - 1
             # apply the bound
-            # print("Ns", Ns)
-            # print("bound", bound(t,Ns))
             curr_avgs = bound(t, Ns) + avg_rewards
             # find the best hand
             best_ind = np.argsort(curr_avgs)[-1]
@@ -95,28 +94,19 @@ class UCB1(Plot):
             N_old = Ns[best_ind]
             Ns[best_ind] += 1
             if adv_seq is None:
-                gain = one_zero_with_prob(probs[best_ind])
+                reward = one_zero_with_prob(probs[best_ind])
                 # update average
-                avg_rewards[best_ind] = (avg_old * N_old + gain) / float((N_old + 1))
+                avg_rewards[best_ind] = updated_average(avg_old, N_old, reward)
+                # avg_rewards[best_ind] = (avg_old * N_old + reward) / float((N_old + 1))
                 # calculate the regret
                 emp_reg = empirical_regret(Ns, (mu_star - probs))
-                regrets[t-1] = emp_reg
+                regrets[t-K-1] = emp_reg
             else:
-                # print("Playing", best_ind)
-                gain = adv_seq[t-K-1, best_ind]
-                # print("t", t)
-                # print("i", t-K-1)
-
-                # print("v",adv_seq[t-K-1])
+                reward = adv_seq[n, best_ind]
                 # update average
-                avg_rewards[best_ind] = (avg_old * N_old + gain) / float((N_old + 1))
+                avg_rewards[best_ind] = updated_average(avg_old, N_old, reward)
                 # calculate the regret
-                # adv_reg = gain - np.min(adv_seq[t-K-1])
-                adv_reg = gain
-                if t == 1:
-                    regrets[0] = adv_reg
-                else:
-                    regrets[t-K-1] = regrets[t-K-2] + adv_reg
+                regrets[n] = regrets[n-1] + reward
         return regrets
 
 
@@ -143,6 +133,8 @@ class EXP3(Plot):
         return self.calc_regrets(self.T, self.K, self.mu_star, self.mu, self.adv_seq)
     def calc_regrets(self, T, K, mu_star, mu, adv_seq):
         # init data
+        adv_seq = np.vstack((np.array([0]*K),adv_seq))[:-1]
+        # print(adv_seq)
         Ls = np.zeros(K)
         ps = np.zeros(K)
         probs = np.array([mu_star] + [mu] * (K-1))
@@ -150,7 +142,8 @@ class EXP3(Plot):
         Ns = np.ones(K)
         regrets = np.zeros(T)
         # iteration
-        for t in range(1, T+1):
+        for t in range(K+1, T+1):
+            n = T - K - 1
             # calculate learning rate
             eta = self.__eta_t(K, t)
             # calculate ps:
@@ -159,14 +152,23 @@ class EXP3(Plot):
             ps = np.exp(ex)/(np.sum(np.exp(ex)))
             # sample hand based on p
             hand = np.random.choice(ps.shape[0], p=ps)
-            # play the hand
-            gain = 1-one_zero_with_prob(probs[hand])
-            l_wave = gain/ps[hand]
-            # update Ls
-            Ls[hand] += l_wave
             Ns[hand] += 1
-            emp_reg = empirical_regret(Ns, (mu_star - probs))
-            regrets[t-1] = emp_reg
+            # play the hand
+            if adv_seq is None:
+                reward = 1-one_zero_with_prob(probs[hand])
+                l_wave = reward/ps[hand]
+                # update Ls
+                Ls[hand] += l_wave
+                emp_reg = empirical_regret(Ns, (mu_star - probs))
+                regrets[n] = emp_reg
+            else:
+                reward = 1-adv_seq[n, hand]
+                l_wave = reward/ps[hand]
+                # update Ls
+                Ls[hand] += l_wave
+                regrets[n] = regrets[n-1] + reward
+            regrets = regrets[:T-K]
+        print("sum", np.sum(regrets))
         return regrets
 
 
@@ -176,6 +178,8 @@ def empirical_regret(N_ts, deltas):
 def one_zero_with_prob(prob):
     return np.random.choice(2, p=[1-prob, prob])
 
+def updated_average(avg_old, N_old, reward):
+    return (avg_old * N_old + reward) / float((N_old + 1))
 
 def plot(T, mu_star, K, mu, n_reps, with_stds=True):
     if debug: print("Plotting for mu={}, K={}".format(mu, K))
@@ -199,9 +203,9 @@ def plot(T, mu_star, K, mu, n_reps, with_stds=True):
 def plot_adv(T, K, n_reps, adv_seq):
     if debug: print("Plotting ADV for K={}".format(K))
     # define plot types
-    # plots = [UCB1_notes(T, mu_star, mu, K, n_reps, "red", adv_seq),
-    #          EXP3(T, mu_star, mu, K, n_reps, "green", adv_seq)]
-    plots = [UCB1_notes(T, 0, 0, K, n_reps, "red", adv_seq)]
+    plots = [UCB1_notes(T, 0, 0, K, n_reps, "red", adv_seq),
+             EXP3(T, 0, 0, K, n_reps, "green", adv_seq)]
+    # plots = [EXP3(T, 0, 0, K, n_reps, "green", adv_seq)]
     # plot each type
     for p in plots:
         if debug: p.print_status()
@@ -219,65 +223,47 @@ def plot_adv(T, K, n_reps, adv_seq):
 def create_adv_seq(T, K):
     def bound(t,N): return np.sqrt((3 * np.log(t))/(2 * N))
     # initialize hands
-    seq = np.zeros(K)
+    seq = []
+    # play the first hand twice and get 1 twice
     avg_rewards = np.array([1] + [0]*(K-1)).astype(float)
     Ns = np.ones(K)
-    regrets = np.zeros(T-K)
     # start building adversarial sequence
     for t in range(K+1, T+1):
-        # print("Ns", Ns)
         # apply the bound
         curr_avgs = bound(t, Ns) + avg_rewards
-        # find the best hand
+        # find the best and worst hands
         best_ind = np.argsort(curr_avgs)[-1]
-        # print("curr_avgs_", curr_avgs)
         worst_ind = np.argsort(curr_avgs)[0]
-        # play the hand
+        # play the hand, but get the reward 0
         avg_old = avg_rewards[best_ind]
         # update times played
         N_old = Ns[best_ind]
         Ns[best_ind] += 1
         # update average
         avg_rewards[best_ind] = (avg_old * N_old) / float((N_old + 1))
-        row = np.zeros(K)
-        row[worst_ind] = 1
-        seq = np.vstack((seq, row))
-
-        # print("t", t)
-        # print("v", row)
-        # print("v", curr_avgs)
-        # print("worst ind", worst_ind)
-        # print("best ind", best_ind)
-        # print("row", row)
-        # adv_reg = seq[T-K-1, best_ind]
-        # if t == 1:
-        #     print("taken")
-        #     regrets[0] = adv_reg
-        # else:
-        #     regrets[t-K-1] = regrets[t-K-2] + adv_reg
-    seq = seq[1:]
-    return seq
+        # add row to the sequence matrix
+        # row = np.zeros(K)
+        # row[worst_ind] = 1
+        row = np.ones(K).astype(float)
+        row[best_ind] = 0
+        seq.append(row)
+    return np.array(seq)
 
 def main():
     T = 10000
+    # T = 20
     mu_star = 0.5
     Ks = [2, 4, 8, 16]
     mu_diffs = [0.25, 0.125, 0.0625]
     mus = [mu_star - mu for mu in mu_diffs]
     # n_reps = 10
     n_reps = 1
-    # T = 10
-    # mu_star = 0.5
-    # Ks = [2, 4, 8, 16]
-    # mu_diffs = [0.25, 0.125, 0.0625]
-    # mus = [mu_star - mu for mu in mu_diffs]
-    # n_reps = 1
 
+    adv_seq = create_adv_seq(T, Ks[1])
 
-    adv_seq = create_adv_seq(T, Ks[0])
-    print("adv_seq.shape", adv_seq.shape)
+    # works v
+    plot_adv(T, Ks[1], n_reps, adv_seq)
 
-    plot_adv(T, Ks[0], n_reps, adv_seq)
     # plot(T, mu_star, Ks[1], mus[1], n_reps)
     # x = EXP3(T, mu_star, mus[1], Ks[1], n_reps, "red")
     # x.plot()
